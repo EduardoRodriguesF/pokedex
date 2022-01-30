db4d105bf8680c64af451bf3dd727400e09081ff
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-01-30 14:21:17 -0300

    setup: pokeapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import api from "./services/api";
+
 function App() {
+  const getPokemon = async () => {
+    await api.get('pokemon/?limit=151').then((response) => {
+      console.log('>>> response', response.data);
+    });
+  }
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
   return (
     <h1>Hello world</h1>
   )
 }
 
-export default App
+export default App;
