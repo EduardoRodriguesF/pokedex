f73034d66031c59298d5bf53b60c8c398ae28288
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-02-05 22:42:21 -0300

    style(PokemonType): reduced border size

--- src/components/PokemonTypes/styles.ts
+++ src/components/PokemonTypes/styles.ts
@@ -14,7 +14,7 @@ export const TypeCard = styled.li<ITypeCardProps>`
   position: relative;
   color: ${({ type }) => tint(0.65, colors.types[type])};
   background: ${({ type }) => rgba(colors.types[type], 0.5)};
-  border: 3px solid ${({ type }) => tint(0.65, colors.types[type])};
+  border: 2px solid ${({ type }) => tint(0.65, colors.types[type])};
   box-sizing: border-box;
   font-weight: 600;
   padding: 6px 16px;
